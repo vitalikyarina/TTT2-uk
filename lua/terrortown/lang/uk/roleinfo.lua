945b538286439cd90fb3ae91154cc0f401357ddd
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-07-06 19:11:41 +0300

    fix roleinfo

---
+++ lua/terrortown/lang/uk/roleinfo.lua
@@ -0,0 +1,14 @@
+L = LANG.GetLanguageTableReference("uk")
+-- TTT2 Interface
+L["role_info_help_menu"] = "Тут ви можете відкрити редактор описів ролей"
+L["roleinfo_addon_header"] = "Додаток для інформації про ролі"
+L["roleinfo_addon_info"] = "Додаток для інформації про ролі"
+L["roleinfo_addon_open_editor"] = "Відкрити редактор"
+-- GUI
+L["roleinfo_addon_editor_title"] = "Описи ролей"
+L["roleinfo_addon_apply"] = "Застосувати зміни"
+L["roleinfo_addon_reset"] = "Скинути всі описи на значення за замовчуванням"
+L["roleinfo_addon_confirmation_title"] = "Будь ласка, підтвердіть"
+L["roleinfo_addon_confirmation"] = "Ви впевнені?"
+L["roleinfo_addon_missing_description"] = "Відсутній опис"
+L["roleinfo_addon_configure_yourself"] = "Налаштуйте його самостійно за допомогою консольної команди 'role_info_gui'"
